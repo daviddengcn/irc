def8e7ebcf7259402ce69fc4c094bbf07f5dee36
David <david@rd.netease.com>
2013-07-24 18:54:10 +0800

    ADD msgonly flag

--- irc.go
+++ irc.go
@@ -3,24 +3,26 @@
 */
 package main
 
-import(
+import (
+	"bufio"
+	"flag"
 	"fmt"
-	"net"
-	"log"
 	"github.com/daviddengcn/go-irc"
 	"github.com/hailiang/gosocks"
-	"bufio"
+	"log"
+	"net"
 	"os"
-	"flag"
 	"strings"
 )
 
 var (
-	proxy   = flag.String("proxy", os.Getenv("IRC_PROXY"), 
+	proxy = flag.String("proxy", os.Getenv("IRC_PROXY"),
 		`Proxy server. If not specified, use environmental variable IRC_PROXY.`)
-	nick     = flag.String("nick", "", `Nick name`)
-	username = flag.String("user", "", `User name`)
+	nick     = flag.String("nick", "Guest", `Nick name`)
+	username = flag.String("user", "User", `User name`)
 	password = flag.String("pass", "", `Password if any`)
+	msgOnly  = flag.Bool("msgonly", false,
+		`Only show messages, no join/quit notifications`)
 	server  = "irc.freenode.net:6667"
 	channel = "#go-nuts"
 )
@@ -28,16 +30,16 @@ var (
 func init() {
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr,
-`  irc [<flags>] [<server:port> [<channel>]]
+			`  irc [<flags>] [<server:port> [<channel>]]
     If <server:port> is not specified or as !, it is set to "irc.freenode.net:6667".
     If <channel> is not set, it is set to "#go-nuts".`)
-	    flag.PrintDefaults()
+		flag.PrintDefaults()
 	}
 }
 
 func main() {
 	flag.Parse()
-	
+
 	if flag.NArg() >= 1 {
 		if flag.Arg(0) != "!" {
 			server = flag.Arg(0)
@@ -46,46 +48,52 @@ func main() {
 	if flag.NArg() >= 2 {
 		channel = flag.Arg(1)
 	}
-	
+
 	var dial func(string, string) (net.Conn, error)
 	if *proxy == "" {
-		fmt.Printf("Connecting %s %s ...\n", server,channel)
+		fmt.Printf("Connecting %s %s ...\n", server, channel)
 		dial = net.Dial
 	} else {
-		fmt.Printf("Connecting %s %s throught proxy %s ...\n", server,channel, *proxy)
+		fmt.Printf("Connecting %s %s throught proxy %s ...\n", server, channel, *proxy)
 		dial = socks.DialSocksProxy(socks.SOCKS5, *proxy)
 	}
-	
+
 	client := irc.NewClient(*nick, *username)
 	client.Password = *password
+	if !*msgOnly {
+		client.DefaultHandler = func(e *irc.Event) {
+			fmt.Printf("%s %+v\n", e.Code, *e)
+		}
+
+		client.SetHandler(irc.JOIN, func(e *irc.Event) {
+			fmt.Printf("> %s has joined\n", e.Nick)
+		})
+		client.SetHandler(irc.QUIT, func(e *irc.Event) {
+			fmt.Printf("> %s has quit\n", e.Nick)
+		})
+
+		client.SetHandler(irc.PART, func(e *irc.Event) {
+			fmt.Printf("> %s leaves channel %s\n", e.Nick, e.Arguments[0])
+		})
+	}
+
 	socket, err := dial("tcp", server)
 	if err != nil {
 		log.Fatal(err)
 	}
 	client.Start(socket)
-	
-	client.DefaultHandler = func(e *irc.Event) {
-		fmt.Printf("%s %+v\n", e.Code, *e)
-	}
-	
+
 	client.SetHandler(irc.RPL_TOPIC, func(e *irc.Event) {
 		fmt.Printf("> Topic: %s\n", e.Message)
 	})
-	client.SetHandler(irc.JOIN, func(e *irc.Event) {
-		fmt.Printf("> %s has joined\n", e.Nick)
-	})
-	client.SetHandler(irc.QUIT, func(e *irc.Event) {
-		fmt.Printf("> %s has quit\n", e.Nick)
-	})
-	
 	client.SetHandler(irc.PRIVMSG, func(e *irc.Event) {
 		fmt.Printf("> %s: %s\n", e.Nick, e.Message)
 	})
-	
+
 	client.SetHandler(irc.RPL_LUSERCHANNELS, func(e *irc.Event) {
 		fmt.Printf("> %s %s\n", e.Arguments[1], e.Message)
 	})
-	
+
 	for _, c := range []string{
 		irc.MODE, irc.NOTICE, irc.ERROR,
 		irc.RPL_YOURHOST, irc.RPL_LUSERCLIENT, irc.RPL_LUSEROP,
@@ -96,20 +104,16 @@ func main() {
 			fmt.Printf("> %s %s\n", e.Code, e.Message)
 		})
 	}
-	
-	client.SetHandler(irc.PART, func(e *irc.Event) {
-		fmt.Printf("> %s leaves channel %s\n", e.Nick, e.Arguments[0])
-	})
 	client.Join(channel)
-	
-	ignored := func(*irc.Event){}
+
+	ignored := func(*irc.Event) {}
 	for _, c := range []string{
 		irc.RPL_NAMREPLY, irc.RPL_ENDOFNAMES, irc.RPL_MOTD, irc.RPL_ENDOFMOTD,
 		irc.RPL_CREATED, irc.RPL_MYINFO, irc.RPL_ISUPPORT,
 	} {
 		client.SetHandler(c, ignored)
 	}
-	
+
 	go func() {
 		rd := bufio.NewReader(os.Stdin)
 		for {
@@ -122,7 +126,7 @@ func main() {
 			client.Privmsg(channel, line)
 		}
 	}()
-	
+
 	if err := client.Serve(); err != nil {
 		log.Fatal(err)
 	}
